5611e31de6e4b80794c0810876b95cc3af351a10
dk.lan <329865849@qq.com>
2017-06-30 11:48:32 +0800

    modify by dk

--- AngularJS1/repeat/common.js
+++ AngularJS1/repeat/common.js
@@ -2,7 +2,16 @@ var commonApp = angular.module('commonApp', []);
 
 commonApp.value('baseUrl', 'http://192.168.1.75/course/api/api/')
 
-commonApp.directive('datagrid', ['$http', 'baseUrl', function($http, baseUrl){
+commonApp.value('lan', function(){
+    var lan = window.localStorage.getItem("lan");
+    if(!lan){
+        lan = "cn";
+    }
+    window.localStorage.setItem("lan", lan);
+    return lan;
+})
+
+commonApp.directive('datagrid', ['$http', 'baseUrl', 'lan', function($http, baseUrl, lan){
     return {
         restrict: 'E',
         templateUrl: 'datagrid.html',
@@ -10,9 +19,22 @@ commonApp.directive('datagrid', ['$http', 'baseUrl', function($http, baseUrl){
         link: function(scope, elment, attr){
             scope.dataset = [];
             console.log(2, scope)
+            
             $http.get(baseUrl + attr.api).success(function(response){
                 scope.dataset = response.data;
+                $http.get('commonDictionary.txt').success(function(response){
+                    scope.commonDictionary = response;
+                    if(attr.dictionary){
+                        $http.get(attr.dictionary).success(function(response){
+                            scope.commonDictionary = Object.assign({}, scope.commonDictionary, response);
+                            // console.log(scope.commonDictionary);
+                        })
+                    }                    
+                })                
             })
+
+            scope.lan = lan();
+            scope.cols = attr.columns ? attr.columns.split(',') : [];
 
             scope.myFilter = function(data){
                 return data;
